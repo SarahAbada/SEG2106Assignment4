c60a4b7ff5cc3e4f0e5e393e8311dcf0d5672df7
Sarah Abada <sabad030@uottawa.ca>
2026-04-13 23:18:10 -0400

    Add multi-threading support for Hill Climbing search
    Implemented multi-threading for Hill Climbing search to solve the N-Queens problem.

--- 8-Queen Problem HC/Main.java
+++ 8-Queen Problem HC/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		
-	       
+			int numThreads = 10;
+			HillClimbingSearch[] hillclimbingsearcharray = new HillClimbingSearch[numThreads];	       
 		    int n = 0; 
 	        try (Scanner s=new Scanner(System.in)) {
 	        	while (true){
@@ -26,15 +26,39 @@ public class Main {
 	        System.out.println("Solution to "+ n +" queens using hill climbing search:");
 
 			ThreadGroup threadgroup = new ThreadGroup("HillClimbingSearchThreadGroup");
+			for (int i=0; i<numThreads; i++){
+				HillClimbingSearch hcs = new HillClimbingSearch(n);
+				hillclimbingsearcharray[i] = hcs;
+				Thread t = new Thread(threadgroup, hcs);
+				t.start();
+			}
+		// 1. Keep looping as long as the "bucket" has active workers
+		while (threadgroup.activeCount() > 0) {
+    
+    		// 2. Peek into each search instance to see if anyone found the answer
+    		for (HillClimbingSearch searchTask : hillclimbingsearcharray) {
+        		if (searchTask.getFinalSolution() != null) {
+            		// 3. Success! Tell the whole group to stop searching
+            		threadgroup.interrupt();
+            		break; 
+        		}
+		    }
+    
+    		// 4. Optional: tiny sleep so the CPU doesn't catch fire checking 1 million times a second
+    		try { Thread.sleep(10); } catch (InterruptedException e) { break; }
+		}
 		
-	        
+	    for (HillClimbingSearch searchTask : hillclimbingsearcharray) {
+    		if (searchTask.getFinalSolution() != null) {
+        		searchTask.printState(searchTask.getFinalSolution());
+        		break;
+    		}
+		}    
 	        //HillClimbingSearch hcs = new HillClimbingSearch(n);
 	        
 	        //hcs.runSearch();
 	        
-	        if (hcs.getFinalSolution() != null)
-	        	hcs.printState(hcs.getFinalSolution());
-	        
+	     
 	        
 	        
 	        //Printing the solution
